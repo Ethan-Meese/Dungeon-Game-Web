3268eea59feffbfaa5c0c18f5f899d36d56342f3
Ethan Meese <meeseethan@gmail.com>
2025-10-12 17:04:17 -0400

    Added a description string to the GameState.cs

--- backend/Core/Models/GameState.cs
+++ backend/Core/Models/GameState.cs
@@ -3,6 +3,7 @@ namespace backend.Core.Models;
 public class GameState
 {
     public string Message { get; set; } = "";
+    public string Description { get; set; } = "";
     public Player Player { get; set; } = new();
     public Room CurrentRoom { get; set; } = new();
     public bool IsGameOver { get; set; } = false;
